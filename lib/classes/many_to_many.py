03194cdb587d26747a61510ee951152919177886
DAVIDMWELU <davidmwelu@gmail.com>
2025-04-25 22:20:45 +0300

    mock challenge coffe shop

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,71 @@
 class Coffee:
     def __init__(self, name):
-        self.name = name
-        
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name is immutable")
+
     def orders(self):
-        pass
-    
+        return [order for order in Order.all if order.coffee == self]
+
     def customers(self):
-        pass
-    
+        return list({order.customer for order in self.orders()})
+
     def num_orders(self):
-        pass
-    
+        return len(self.orders())
+
     def average_price(self):
-        pass
+        prices = [order.price for order in self.orders()]
+        return sum(prices) / len(prices) if prices else 0
+
 
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise Exception("Name must be a string between 1 and 15 characters.")
+
     def orders(self):
-        pass
-    
+        return [order for order in Order.all if order.customer == self]
+
     def coffees(self):
-        pass
-    
-    def create_order(self, coffee, price):
-        pass
-    
+        return list({order.coffee for order in self.orders()})
+
+
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
+        self._price = None
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if isinstance(value, float) and 1.0 <= value <= 10.0:
+            if self._price is None:
+                self._price = value
+            else:
+                raise AttributeError("Price is immutable")
+        else:
+            raise Exception("Price must be float between 1.0 and 10.0")
